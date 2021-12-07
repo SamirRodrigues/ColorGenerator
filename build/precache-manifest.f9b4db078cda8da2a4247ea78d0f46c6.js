cfbcabaf634ccb1ef1f3396c77952b0f57007563
SamirRodrigues <emaildesamir@gmail.com>
2021-12-06 21:21:15 -0300

    deploy

---
+++ build/precache-manifest.f9b4db078cda8da2a4247ea78d0f46c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979f2ab7c38a2a3d817d30a2454f75bc",
+    "url": "/ColorGenerator/index.html"
+  },
+  {
+    "revision": "a754167c1a16281db109",
+    "url": "/ColorGenerator/static/css/main.39127fa9.chunk.css"
+  },
+  {
+    "revision": "0b728d42613a7c9ec6d3",
+    "url": "/ColorGenerator/static/js/2.cdd6e70d.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/ColorGenerator/static/js/2.cdd6e70d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a754167c1a16281db109",
+    "url": "/ColorGenerator/static/js/main.947bbd36.chunk.js"
+  },
+  {
+    "revision": "5cd393ef5cfd7c35de60",
+    "url": "/ColorGenerator/static/js/runtime-main.6a8392e4.js"
+  }
+]);
